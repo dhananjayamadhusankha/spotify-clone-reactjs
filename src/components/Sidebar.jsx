7e3dd800d56b9b2e73c69cf115903236e778c2f6
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-06-01 20:39:49 +0530

    sidebar is completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,35 +12,45 @@ import React from "react";
 // bg-blue-500 h-[15%] rounded flex flex-col justify-around
 const Sidebar = () => {
   return (
-    <div className="text-gray-500 p-5 border-r border-gray-900 bg-[#121212]">
+    <div className="text-gray-500 p-5 border-r border-gray-900">
       <div className="space-y-4">
-        <div className="flex items-center space-x-2 hover:text-white cursor-pointer">
+        <button className="flex items-center space-x-2 hover:text-white">
           <Home className="h-5 w-5" />
           <p className="font-semibold">Home</p>
-        </div>
-        <div className="flex items-center space-x-2 hover:text-white cursor-pointer">
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
           <SearchIcon className="h-5 w-5" />
           <p className="font-semibold">Search</p>
-        </div>
-        <div className="flex items-center space-x-2 hover:text-white cursor-pointer">
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
           <LibraryIcon className="h-5 w-5" />
           <p className="font-semibold">Your Library</p>
-        </div>
+        </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-        {/* --------------------------------------------------------------------------------- */}
-        <div className="flex items-center space-x-2 hover:text-white cursor-pointer">
+
+        <button className="flex items-center space-x-2 hover:text-white">
           <CirclePlus className="h-5 w-5" />
           <p className="font-semibold">Create Playlist</p>
-        </div>
-        <div className="flex items-center space-x-2 hover:text-white cursor-pointer">
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
           <Rss className="h-5 w-5" />
           <p className="font-semibold">Your episodes</p>
-        </div>
-        <div className="flex items-center space-x-2 hover:text-white cursor-pointer">
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
           <HeartIcon className="h-5 w-5" />
           <p className="font-semibold">Liked Songs</p>
-        </div>
+        </button>
         <hr className="border-t-[0.1px] border-gray-900" />
+      </div>
+
+      <div className="bg-gray-500 p-3 space-y-3 rounded-md text-white">
+        <div>
+          <p className="font-semibold">Let's findsome podcasts to follow</p>
+          <p>We'll keep you update on new episodes</p>
+        </div>
+        <button className="px-4 py-2 rounded-full bg-white font-semibold hover:border-2 hover:bg-gray-500 hover:text-white text-black">
+          Brows podcasts
+        </button>
       </div>
     </div>
   );
