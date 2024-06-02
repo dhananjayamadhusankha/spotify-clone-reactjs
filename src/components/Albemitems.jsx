58fcc564be9291638b81302cef8dc75d0c7bddc7
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-06-02 19:52:04 +0530

    implement Featured Charts and Today's Big Hits in Displaypage

---
+++ src/components/Albemitems.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Albemitems = ({ id, name, image, desc }) => {
+  console.log("id>>>", id)
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate(`/album/${id}`)}
+      className="min-w-[180px] p-2 px-3 rounded-lg cursor-pointer hover:bg-[#ffffff26] w-fit"
+    >
+      <img className="rounded-lg" src={image} alt={name} />
+      <p className="font-bold mt-2 mb-1">{name}</p>
+      <p className="text-slate-200 text-sm">{desc}</p>
+    </div>
+  );
+};
+
+export default Albemitems;
--- src/components/DisplayPage.jsx
+++ src/components/DisplayPage.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/assets";
+import Albemitems from "./Albemitems";
+import SongsItems from "./SongsItems";
 
 const DisplayPage = () => {
   return (
-    <><Navbar /></>
-  )
-}
+    <>
+      <Navbar />
+      <div className="my-4">
+        <h1 className="font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <Albemitems
+              key={index}
+              id={item.id}
+              image={item.image}
+              desc={item.desc}
+              name={item.name}
+            />
+          ))}
+        </div>
+        <h1 className="font-bold text-2xl">Today's Big Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongsItems
+              key={index}
+              id={item.id}
+              image={item.image}
+              desc={item.desc}
+              name={item.name}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default DisplayPage
+export default DisplayPage;
