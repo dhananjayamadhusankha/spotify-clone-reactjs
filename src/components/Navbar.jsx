5e9c3a478226c4f02d25a2413d67ace7ed515a0d
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-06-02 16:01:33 +0530

    Navbar component is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,17 @@ const Navbar = () => {
           <ChevronLeft className="bg-black cursor-pointer rounded-full w-8 h-8 p-1 hover:bg-slate-700" />
           <ChevronRight className="bg-black cursor-pointer rounded-full w-8 h-8 p-1 hover:bg-slate-700" />
         </div>
-        <div className="flex gap-3">
+        <div className="flex gap-3 items-center">
           <button className=" text-sm px-4 py-2 bg-white text-black rounded-full hidden md:block hover:bg-gray-200">Explore Premium</button>
           <button className=" text-sm px-4 py-2 bg-black text-white rounded-full border-white border hover:text-black hover:bg-white hover:border-white">Install App</button>
+          <p className="flex bg-purple-500 w-8 h-8 rounded-full justify-center items-center cursor-pointer hover:bg-purple-600">D</p>
         </div>
       </div>
+        <div className=" flex gap-4 items-center mt-4">
+          <button className=" bg-white px-4 py-1 text-black rounded-full cursor-pointer">All</button>
+          <button className="px-4 py-1 rounded-full text-gray-500 cursor-pointer hover:text-white">Music</button>
+          <button className="px-4 py-1 rounded-full text-gray-500 cursor-pointer hover:text-white">Podcasts</button>
+        </div>
     </>
   );
 };
